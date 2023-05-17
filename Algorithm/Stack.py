b8891915521c1566043667933a7e78b6aa7fb592
Patel <janmesh04@gmail.com>
2023-05-16 20:07:14 -0400

    Adding code in stack file

---
+++ Algorithm/Stack.py
@@ -0,0 +1,26 @@
+def push(num, n):
+    num.append(n)
+    return num
+
+def pop(num):
+    if isEmpty(num):
+        print("Stack is Empty.")
+        return False
+    else :
+        num.pop()
+        return num
+
+def isEmpty(num):
+    if not num:
+        return True
+    else :
+        return False
+    
+num = []
+push(num,2)
+push(num,3)
+push(num,1)
+push(num,4)
+pop(num)
+push(num,3)
+print(num)
